565ff4e708cffd0073d2f11e0e128be59bd419d8
Enzo Nicolas Stagno Piriz <49207091+mcprog1@users.noreply.github.com>
2022-03-24 14:26:00 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  //Colocar future builder para obtener las categorias y despues redenrizar el MaterialApp
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
